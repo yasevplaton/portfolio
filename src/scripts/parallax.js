6d6b4a8f007ab5e3da49b2fc6d9336bd75c76b7a
yasevplaton <yasevplaton@gmail.com>
2019-03-24 00:19:57 +0300

    add draft parallax effect to greeting section

---
+++ src/scripts/parallax.js
@@ -0,0 +1,21 @@
+// GREETING PARALLAX
+
+const greetParallax = document.querySelector(".greeting-parallax");
+const layers = greetParallax.children;
+
+function moveLayers(wScroll) {
+  
+  Array.from(layers).forEach(layer => {
+
+    const speedCoeff = layer.dataset.speed / 10;
+    const layerOffset = wScroll * speedCoeff;
+
+    layer.style.transform = `translateY(-${layerOffset}%)`;
+  })
+
+}
+
+window.addEventListener('scroll', e => {
+  const wScroll = window.pageYOffset;
+  moveLayers(wScroll);
+});
