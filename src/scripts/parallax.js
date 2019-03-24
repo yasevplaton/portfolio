fd50d5a2a84e1b68f25fc1ac3b90e062602142f4
yasevplaton <yasevplaton@gmail.com>
2019-03-24 10:52:28 +0300

    fix some bugs in greeting parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,14 +1,21 @@
 // GREETING PARALLAX
 
 const greetParallax = document.querySelector(".greeting-parallax");
+const greetParallaxHeight = getComputedStyle(greetParallax).height;
+console.log(greetParallaxHeight);
 const layers = greetParallax.children;
 
 function moveLayers(wScroll) {
+  console.log(wScroll);
   
   Array.from(layers).forEach(layer => {
 
-    const speedCoeff = layer.dataset.speed / 10;
+    const speedCoeff = layer.dataset.speed;
     const layerOffset = wScroll * speedCoeff;
+
+    // if (wScroll < 350) {
+    //   layer.style.transform = `translateY(-${layerOffset}%)`;
+    // }
 
     layer.style.transform = `translateY(-${layerOffset}%)`;
   })
