5ba435060253fc0cf0d5a3d9a0f9c9636f01607a
yasevplaton <yasevplaton@gmail.com>
2019-03-24 13:24:31 +0300

    create template for skills widget

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -4,16 +4,12 @@ const greetParallax = document.querySelector(".greeting-parallax");
 const layers = greetParallax.children;
 
 function moveLayers(wScroll) {
-  console.log(wScroll);
   
   Array.from(layers).forEach(layer => {
 
     const speedCoeff = layer.dataset.speed;
     const layerOffset = wScroll * speedCoeff;
 
-    // if (wScroll < 350) {
-    //   layer.style.transform = `translateY(-${layerOffset}%)`;
-    // }
 
     layer.style.transform = `translateY(-${layerOffset}%)`;
   })
