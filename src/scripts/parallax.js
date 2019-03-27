7252b9827abc93af40bedfe263c89238636e7e8d
yasevplaton <yasevplaton@gmail.com>
2019-03-27 12:13:41 +0300

    add layers to budda parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,9 +1,14 @@
 // GREETING PARALLAX
 
 const greetParallax = document.querySelector(".greeting-parallax");
-const layers = greetParallax.children;
+const greetLayers = greetParallax.children;
 
-function moveLayers(wScroll) {
+const revContParallax = document.querySelector(".rev-cont-parallax");
+const revContLayers = revContParallax.children;
+
+
+
+function moveLayers(wScroll, layers) {
   Array.from(layers).forEach(layer => {
     const speedCoeff = layer.dataset.speed;
     const layerOffset = (wScroll * speedCoeff) / 30;
@@ -15,6 +20,7 @@ function moveLayers(wScroll) {
 window.addEventListener("scroll", e => {
   if (window.innerWidth >= 768) {
     const wScroll = window.pageYOffset;
-    moveLayers(wScroll);
+    console.log(wScroll);
+    moveLayers(wScroll, greetLayers);
   }
 });
