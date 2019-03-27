7628cf0aeff9a16904068e7137b455ddfa4d855f
yasevplaton <yasevplaton@gmail.com>
2019-03-27 20:09:02 +0300

    realize budda parallax

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -1,12 +1,9 @@
-// GREETING PARALLAX
-
 const greetParallax = document.querySelector(".greeting-parallax");
 const greetLayers = greetParallax.children;
 
 const revContParallax = document.querySelector(".rev-cont-parallax");
 const revContLayers = revContParallax.children;
-
-
+let revContTop, delta;
 
 function moveLayers(wScroll, layers) {
   Array.from(layers).forEach(layer => {
@@ -18,9 +15,41 @@ function moveLayers(wScroll, layers) {
 }
 
 window.addEventListener("scroll", e => {
+
   if (window.innerWidth >= 768) {
+
+    if (!revContTop) {
+      revContTop = getCoords(revContParallax).top;
+      delta = revContTop - window.innerHeight;
+    }
+
+
     const wScroll = window.pageYOffset;
-    console.log(wScroll);
     moveLayers(wScroll, greetLayers);
+    
+    console.log(wScroll, delta);
+
+    if (wScroll > delta) {
+      const newScroll = wScroll - delta;
+      moveLayers(newScroll, revContLayers);
+    }
   }
 });
+
+function getCoords(elem) {
+  var box = elem.getBoundingClientRect();
+
+  var body = document.body;
+  var docEl = document.documentElement;
+
+  var scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+  var scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+
+  var clientTop = docEl.clientTop || body.clientTop || 0;
+  var clientLeft = docEl.clientLeft || body.clientLeft || 0;
+
+  var top  = box.top +  scrollTop - clientTop;
+  var left = box.left + scrollLeft - clientLeft;
+
+  return { top: Math.round(top), left: Math.round(left) };
+}
