87c9806773f4cc9c45cdf6a3cc1396bfd5b4d7d4
yasevplaton <yasevplaton@gmail.com>
2019-03-27 10:37:45 +0300

    enable parallax only for tablets and more

--- src/scripts/parallax.js
+++ src/scripts/parallax.js
@@ -4,21 +4,17 @@ const greetParallax = document.querySelector(".greeting-parallax");
 const layers = greetParallax.children;
 
 function moveLayers(wScroll) {
-  
   Array.from(layers).forEach(layer => {
-
     const speedCoeff = layer.dataset.speed;
-    const layerOffset = wScroll * speedCoeff / 30;
-
+    const layerOffset = (wScroll * speedCoeff) / 30;
 
     layer.style.transform = `translateY(-${layerOffset}%)`;
-  })
-
-}
-
-if (window.innerWidth >= 768) {
-  window.addEventListener('scroll', e => {
-    const wScroll = window.pageYOffset;
-    moveLayers(wScroll);
   });
 }
+
+window.addEventListener("scroll", e => {
+  if (window.innerWidth >= 768) {
+    const wScroll = window.pageYOffset;
+    moveLayers(wScroll);
+  }
+});
