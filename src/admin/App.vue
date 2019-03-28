e9a8bb99b54145d2f06cb893989da71f00ca93fc
yasevplaton <yasevplaton@gmail.com>
2019-03-28 23:11:47 +0300

    markup about section and add some styles to it

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -1,3 +1,11 @@
 <template lang="pug">
-  h1 Welcome to the Vue App
+  .content-wrapper
+    include ./pug-templates/header.pug
+    include ./pug-templates/nav.pug
+    include ./pug-templates/about.pug
 </template>
+
+<style lang="postcss">
+@import "./styles/base.pcss";
+@import "./styles/blocks/*.pcss";
+</style>
