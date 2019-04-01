7eb26684f5de4b32f68c89dc66fd0eda2da350c6
Platon Yasev <yasevplaton@gmail.com>
2019-04-01 10:35:31 +0300

    import mixins to admin panel

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -397,6 +397,7 @@
 <style lang="postcss">
 @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,400i,600,700,800&subset=cyrillic');
 @import "normalize.css";
+@import "../styles/mixins.pcss";
 @import "./styles/base.pcss";
 @import "./styles/blocks/*.pcss";
 </style>
