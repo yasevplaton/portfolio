1c69cf1d8e2b145a7fd0ec3232e9fb23dc0f7eee
Platon Yasev <yasevplaton@gmail.com>
2019-04-02 21:03:00 +0300

    update dist folder

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import "./styles/main.pcss";
-if (process.env.NODE_ENV === "development") {
-  require("file-loader!./index.pug");
-}
-
-import "./scripts/parallax";
-import "./scripts/skills";
-import "./scripts/works";
-import "./scripts/reviews";
