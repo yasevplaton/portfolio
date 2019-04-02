86ea88e9487d30d591572919e6d6d7ae508d4339
Platon Yasev <yasevplaton@gmail.com>
2019-04-02 20:55:42 +0300

    add login form validation

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -6,24 +6,42 @@
       .login__content
         a(href="#").btn.btn--close-login-window
         h1.login__title Авторизация
-        form.login__form
+        form.login__form(@submit.prevent="login")
           .login__form-row
             .login__form-col
-              login-input
+              login-input(
+                title="Логин"
+                v-model="user.name"
+                type="text"
+                class="login-input__block--login"
+                name="login"
+                :errorText="validation.firstError('user.name')"
+              )
           .login__form-row
             .login__form-col
-              login-input
+              login-input(
+                title="Пароль"
+                v-model="user.password"
+                type="password"
+                class="login-input__block--password"
+                name="password"
+                :errorText="validation.firstError('user.password')"
+              )
           .login__form-row
             .login__form-col
-              button(type="button").btn.btn__login-submit Отправить
+              button(
+                type="submit"
+                :disabled="disableSubmit"
+              ).btn.btn__login-submit Отправить
     
-    // include ./admin-template.pug
+    include ./admin-template.pug
       
 </template>
 
 <script>
 import { Validator } from "simple-vue-validator";
 import axios from "axios";
+import loginInput from "./components/login/loginInput";
 
 
 export default {
@@ -46,7 +64,7 @@ export default {
     };
   },
   components: {
-    loginInput: () => import("./components/login/loginInput")
+    loginInput
   },
   methods: {
     async login() {
@@ -77,4 +95,64 @@ export default {
 @import "../styles/mixins.pcss";
 @import "./styles/base.pcss";
 @import "./styles/blocks/*.pcss";
+
+/* login component */
+
+.login {
+  background-image: linear-gradient(rgba(#2d3c4e, 0.9), rgba(#2d3c4e, 0.9)),
+    url("../images/bg/mountain-baloon.jpg");
+  background-position: center center, center center;
+  background-repeat: repeat, no-repeat;
+  background-size: cover, cover;
+  min-height: 100vh;
+  min-width: 100vw;
+  position: relative;
+
+  @include phones {
+    background: #fff;
+  }
+}
+
+.login__content {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  background: #fff;
+  padding: 50px 75px;
+
+  @include phones {
+    padding: 20% 15%;
+  }
+}
+
+.login__title {
+  font-size: 36px;
+  font-weight: 600;
+  color: $text-color;
+  margin-bottom: 30px;
+  text-align: center;
+
+  @include phones {
+    font-size: 30px;
+  }
+}
+
+.login__form {
+  width: 100%;
+}
+
+.login__form-row {
+  width: 100%;
+  display: flex;
+  margin-bottom: 40px;
+
+  &:last-child {
+    margin-bottom: 0;
+  }
+}
+
+.login__form-col {
+  flex: 1;
+}
 </style>
